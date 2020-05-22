c6052fc4b242f78aa70f098a042265a10dae7d79
Cesar Lara <cesar.lara@yale.edu>
2020-05-22 15:04:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,9 @@ def game_hash
   #has two keys :home and :away, which in turn have hashes
   home: {team_name: "Brooklyn Nets",
         colors: ["Black", "White"],
-        players: [{player_name: "Alan Anderson",
+        players: [
+
+          {player_name: "Alan Anderson",
                    number: 0,
                    shoe:  16,
                    points: 22,
@@ -13,8 +15,22 @@ def game_hash
                    assists: 12,
                    steals:  3,
                    blocks:  1,
-                   slam_dunks: 1
-}]
+                   slam_dunks: 1},
+
+
+            {player_name: "Alan Anderson",
+                  number: 0,
+                  shoe:  16,
+                  points: 22,
+                  rebounds:  12,
+                  assists: 12,
+                  steals:  3,
+                  blocks:  1,
+                  slam_dunks: 1},
+                   
+
+
+]
 
 
 
