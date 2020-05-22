bdc8bffe0f32f6de2276d2123342a20266a8cec4
Cesar Lara <cesar.lara@yale.edu>
2020-05-22 15:11:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,7 +37,7 @@ def game_hash
                     blocks:  1,
                     slam_dunks: 15},
 
-              {player_name: "Alan Anderson",
+              {player_name: "Mason Plumlee",
                       number: 0,
                       shoe:  16,
                       points: 22,
@@ -47,7 +47,7 @@ def game_hash
                       blocks:  1,
                       slam_dunks: 1},
 
-                  {player_name: "Alan Anderson",
+                  {player_name: "Jason Terry",
                       number: 0,
                       shoe:  16,
                       points: 22,
